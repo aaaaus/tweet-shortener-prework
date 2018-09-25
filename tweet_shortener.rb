25a02254794a59c0af4c01895bf61e5597c29c17
Austin Luft <ausluft@gmail.com>
2018-09-25 19:53:02 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,14 @@
 # Write your code here.
 
-def dictionary {
-  
-  
-  
+def dictionary 
+  {
+  "hello" => "hi"
+  ["to", "two", "too"] => "2"
+  ["for", "four"] => "4"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"
 }
 end
 
